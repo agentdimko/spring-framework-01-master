017137898efdb189b758b4343847c679fce102ae
agentdimko <dimanew2004@mail.ru>
2019-07-30 19:00:39 +0300

    with property injection

--- src/main/java/ru/otus/spring01/service/PersonServiceImpl.java
+++ src/main/java/ru/otus/spring01/service/PersonServiceImpl.java
@@ -7,9 +7,13 @@ public class PersonServiceImpl implements PersonService {
 
     private PersonDao dao;
 
-    public PersonServiceImpl(PersonDao dao) {
+    public void setDao(PersonDao dao) {
         this.dao = dao;
     }
+
+//    public PersonServiceImpl(PersonDao dao) {
+//        this.dao = dao;
+//    }
 
     public Person getByName(String name) {
         return dao.findByName(name);
